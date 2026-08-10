b9395540ea6c5cda7bf68e9527548dc13004fef1
Maithali <maithalygharde1997@gmail.com>
2026-08-10 23:44:47 +0530

    feat: add Employee abstract contract and Developer implementation for role retrieval

---
+++ contracts/EmployeeAbstract.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+abstract contract Employee {
+    string public company = "Blockchain Company";
+
+    function getRole() public virtual returns(string memory);
+
+    function getCompany() public view returns(string memory){
+        return company;
+    }
+
+
+}
+
+contract Developer is Employee {
+    function getRole() public pure override returns(string memory){
+        return "Blockchain Developer"
+    }
+}
