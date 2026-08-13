7cc6e9c4fa47c95823630c4d9133d0cb440b0061
Maithali <maithalygharde1997@gmail.com>
2026-08-13 15:48:02 +0530

    feat: add UserBalance contract to manage user balances and deposits

---
+++ contracts/UserBalance.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifer: MIT
+pragma solidity ^0.8.20;
+
+contract UserBalance{
+    mapping(address => uint) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function getMyBalance() public view returns(uint){
+        return balances[msg.sender];
+    }
+
+    function getContractBalance() public view returns(uint){
+        return address(this).balance;
+    }
+}
