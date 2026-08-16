3aeae3b44f023eddf8e4188dcef0a979b4ae0ba7
Maithali <maithalygharde1997@gmail.com>
2026-08-16 14:59:30 +0530

    feat: add VulnerableBank contract with deposit and withdraw functions

---
+++ contracts/VulnerableBank.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract VulnerableBank{
+
+    mapping(address => uint) public balances;
+
+    function deposit() public payable{
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint amount) public {
+        require( balances[msg.sender] >=amount, "Insufficient balance");
+
+        // External call BEFORE state update
+        (bool success,)= payable(msg.sender).call{value: amount}("");
+
+        require(success, "Transfer failed");
+
+        // State update happens too late
+        balances[msg.sender] -= amount;
+    }
+}
