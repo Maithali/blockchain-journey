1b1ff8dfc442eaba5c2e79c3e27773a82ca8b47d
Maithali <maithalygharde1997@gmail.com>
2026-08-16 15:14:38 +0530

    feat: add SecureBank contract for managing deposits and withdrawals

---
+++ contracts/SecureBank.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SecureBank {
+    mapping(address => uint ) public balances;
+    function deposit() public payable{
+        require(msg.value > 0, "Deposit must be greater tha zero");
+
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint amount) public  {
+        //CHECK
+        require(balances[msg.sender] >= amount,"Insufficient balances");
+
+        //EFFECT
+        balances[msg.sender] -= amount;
+
+        // INTERACTION
+        (bool success, ) = payable(msg.sender).call{value:amount}("");
+
+        require(success,"Transfer failed");
+    }
+}
