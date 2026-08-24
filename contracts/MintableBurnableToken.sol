9d6183da7bff5a2a4da1e5892f29985afe72f5ef
Maithali <maithalygharde1997@gmail.com>
2026-08-24 18:30:12 +0530

    docs: add detailed deployment and usage instructions for MintableBurnableToken

--- contracts/MintableBurnableToken.sol
+++ contracts/MintableBurnableToken.sol
@@ -18,3 +18,25 @@ contract MintableBurnableToken is ERC20, ERC20Burnable, Ownable {
         _mint(to, amount);
     }
 }
+
+/*
+In the input box next to the Deploy button (labeled initialOwner), paste that address.
+ then deploy
+ 1)Click owner -> verifies your deployer address
+ 2)Click totalSupply-> shows 500000000000000000000000 
+ 3)Paste your deployer address into balanceOf -> confirms you hold all 500,000 initial tokens.
+ 4)Mint new tokens (Owner Only)
+    . account#2 address copy paste in to address and amount transact. only owner can mint.
+    Check balanceOf(Account #2).
+    check total amount it has increased.
+
+    Test owner restriction security
+    Switch your active Account dropdown at the top to Account #2 (a non-owner).
+    Try to call mint to give any address more tokens.
+    Click transact -> the transaction reverts with an OwnableUnauthorizedAccount error because Account #2 is not the owner.
+5) 5.Burn tokens (Using ERC20Burnable):Stay logged into Account #2.
+   Expand burn.In amount: enter amount.
+   Click transact.
+   Re-check balanceOf(Account #2) -> drops.
+    totalSupply permanently drops.
+    */
